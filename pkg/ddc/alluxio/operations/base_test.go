1be66862617e38853df9b3854ae9b56603d269b5
biubiu-biub <616119430@qq.com>
2020-09-08 23:39:14 +0800

    modify config

--- pkg/ddc/alluxio/operations/base_test.go
+++ pkg/ddc/alluxio/operations/base_test.go
@@ -4,10 +4,13 @@ import (
 	"errors"
 	"github.com/brahma-adshonor/gohook"
 	"github.com/fluid-cloudnative/fluid/pkg/utils/kubeclient"
-	tt "github.com/go-logr/logr/testing"
+	ltest "github.com/go-logr/logr/testing"
+
 	"strings"
 	"testing"
 )
+
+
 
 func TestAlluxioFileUtils_IsExist(t *testing.T) {
 	mockExecTramp := func(p1, p2, p3 string, p4 []string) (stdout string, stderr string, e error) {
@@ -22,6 +25,7 @@ func TestAlluxioFileUtils_IsExist(t *testing.T) {
 	}
 
 	mockExec := func(p1, p2, p3 string, p4 []string) (stdout string, stderr string, e error) {
+
 		if strings.Contains(p4[3], "not-exist") {
 			return "does not exist", "", errors.New("does not exist")
 		} else if strings.Contains(p4[3], "other-err") {
@@ -35,7 +39,8 @@ func TestAlluxioFileUtils_IsExist(t *testing.T) {
 	if err != nil {
 		t.Fatal(err.Error())
 	}
-	l := tt.NullLogger{}
+
+
 	var tests = []struct {
 		in  string
 		out bool
@@ -46,13 +51,13 @@ func TestAlluxioFileUtils_IsExist(t *testing.T) {
 		{"fine", true, nil},
 	}
 	for _, test := range tests {
-		found, err := AlluxioFileUtils{log: l}.IsExist(test.in)
+		found, err := AlluxioFileUtils{log: ltest.NullLogger{}}.IsExist(test.in)
 
 		if found != test.out {
 			t.Errorf("input parameter is %s,expected %t, got %t", test.in, test.out, found)
 		}
 		if test.err == nil && err != nil {
-			t.Errorf("input parameter is %s,and err should be nil", test.in)
+			t.Errorf("input parameter is %s,and err should be nil,output is %s", test.in,err.Error())
 		}
 		if test.err != nil && err == nil {
 			t.Error("wrong")
